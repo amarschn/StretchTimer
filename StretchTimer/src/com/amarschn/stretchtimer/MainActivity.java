7b26c59b38d9ba8690b676f84d66eb36a24c17b4
amarschn <andrew.marschner@gmail.com>
2014-08-13 17:10:28 -0400

    - Implemented a random stretch set method, to give a set of random stretches - Timer resets to zero once stretch set iterator has been exhausted - Added a few stretches

--- StretchTimer/src/com/amarschn/stretchtimer/MainActivity.java
+++ StretchTimer/src/com/amarschn/stretchtimer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.amarschn.stretchtimer;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Set;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -17,7 +19,8 @@ public class MainActivity extends Activity {
 	private Button startButton;
 	private TextView timerTextView;
 	private TextView stretchText;
-	private String[] stretches;
+	private String[] allStretches;
+	private Iterator<String> stretchesToDo;
 	private long startTime = 0;
 
 	/* The timer thread and handler */
@@ -32,14 +35,24 @@ public class MainActivity extends Activity {
 			int minutes = seconds / 60;
 			seconds = seconds % 60;
 			// Set the text view
-			timerTextView.setText(String.format("%02d:%02d:%02d", minutes,
-					seconds, timeSinceStart % 10));
+			timerTextView.setText(String.format("%02d:%02d", minutes, seconds));
 			// Add the timer to the run queue
 			timerHandler.postDelayed(this, 100);
 
-			if (seconds > 30) {
-				startTime = System.currentTimeMillis();
-				stretchText.setText(getRandomStretch());
+			/*
+			 * If the time is over the set amount of time for the given stretch,
+			 * move on to the next stretch. If there is no stretch, set the
+			 * clock back to zero.
+			 */
+			if (seconds > 5) {
+				if (stretchesToDo.hasNext()) {
+					startTime = System.currentTimeMillis();
+					stretchText.setText(stretchesToDo.next());
+				} else {
+					timerHandler.removeCallbacks(timerRunner);
+					timerTextView.setText(R.string.zero_time);
+					stretchText.setText(R.string.stretch_type);
+				}
 			}
 		}
 	};
@@ -49,8 +62,8 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		/* Set the stretches array */
-		stretches = getResources().getStringArray(R.array.stretches_array);
+		/* Set the stretches array, and set the number of stretches to do */
+		allStretches = getResources().getStringArray(R.array.stretches_array);
 
 		/* Set the timer */
 		timerTextView = (TextView) findViewById(R.id.timerView);
@@ -58,7 +71,7 @@ public class MainActivity extends Activity {
 
 		/* Set the stretch text view */
 		stretchText = (TextView) findViewById(R.id.stretchName);
-
+		stretchText.setText(R.string.stretch_type);
 		/* Set the button listener */
 		startButton = (Button) findViewById(R.id.button1);
 		startButton.setOnClickListener(new View.OnClickListener() {
@@ -69,10 +82,12 @@ public class MainActivity extends Activity {
 				 * Reset the text in the text field below the button to
 				 * something
 				 */
+				stretchText.setText(stretchesToDo.next());
 
-				stretchText.setText(getRandomStretch());
+				/* Reset the stretches to perform */
+				stretchesToDo = getRandomStretchList(5).iterator();
 
-				/* Start the timer again */
+				/* Start the timer */
 				startTime = System.currentTimeMillis();
 				timerHandler.postDelayed(timerRunner, 0);
 			}
@@ -86,16 +101,24 @@ public class MainActivity extends Activity {
 	private String getRandomStretch() {
 		/* Get random integer for index */
 		Random rand = new Random();
-		int range = stretches.length;
+		int range = allStretches.length;
 		int randIndex = rand.nextInt(range);
-		return stretches[randIndex];
+		return allStretches[randIndex];
 	}
 
 	/**
-	 * @return a list of (semi)random stretches
+	 * @param numStretches
+	 *            : The number of stretches that will be returned
+	 * @returna list of (semi)random stretches
 	 */
-	private List<String> getRandomStretchList() {
-		return null;
+	private Set<String> getRandomStretchList(int numStretches) {
+		Set<String> stretches = new HashSet<String>();
+		/* Add random stretches to the set until you have the number requested */
+		while (stretches.size() < numStretches) {
+			String stretch = getRandomStretch();
+			stretches.add(stretch);
+		}
+		return stretches;
 	}
 
 	@Override
